9889bf5b13844ddbf3dd5e21e89c6175a943b034
Mir Neamul Karim Auvik <auvik1@yahoo.com>
2017-10-12 15:19:54 +0600

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './App.css'
+
+import YourComponent from 'components/HamburgerButton'
+
+class App extends Component {
+  render() {
+    return <YourComponent />
+  }
+}
+
+export default App
