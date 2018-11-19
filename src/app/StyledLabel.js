829052107995b9e708d073413bae76f0d723c1b7
Mir Neamul Karim <auvik1@yahoo.com>
2018-11-20 00:20:18 +0600

    changed demo page to dark

--- src/app/StyledLabel.js
+++ src/app/StyledLabel.js
@@ -1,7 +1,8 @@
 import styled from 'styled-components'
 
 export const StyledLabel = styled.div`
-  margin: 15px 0;
+  margin: 15px 0 30px;
+  color: white;
 `
 
 StyledLabel.displayName = 'StyledLabel'
